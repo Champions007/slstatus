55de017e05c7a3bdf7df2bc386156e6fc4faa770
Champions007 <mareksrops53#gmail.com>
2020-07-30 03:14:13 +0300

    Add pc memory usage indicator.

--- config.h
+++ config.h
@@ -63,7 +63,8 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%a %b %d %r" },
+	{ datetime, "%s ",           "%a %b %d %r" },
+    { ram_perc, " MEM %s",           "NULL"},
     { battery_perc, " BAT %s",       "BAT0" },
     { run_command, " VOL %s",        "pamixer --get-volume" },
 };
